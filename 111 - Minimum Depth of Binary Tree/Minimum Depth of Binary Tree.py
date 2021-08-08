b683a08757fd3024cf99b7ae619ce519d500c38d
christopher-liu <christopherbrianliu@gmail.com>
2021-08-07 17:56:24 -0700

    Add solution to 111 problem

--- 111 - Minimum Depth of Binary Tree/Minimum Depth of Binary Tree.py
+++ 111 - Minimum Depth of Binary Tree/Minimum Depth of Binary Tree.py
@@ -4,3 +4,14 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        
+        if not root:
+            return 0
+        elif not root.left or not root.right:
+            return max(self.minDepth(root.left), self.minDepth(root.right)) + 1
+        else:
+            return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+        
